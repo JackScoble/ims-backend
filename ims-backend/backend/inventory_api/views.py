2833378087e9dcf01875745611052e8f35ddf6fa
JackScoble <jack.scoble@ada.ac.uk>
2026-02-20 14:59:35 +0000

    Restrict item deletion to owners in perform_destroy

--- ims-backend/backend/inventory_api/views.py
+++ ims-backend/backend/inventory_api/views.py
@@ -6,13 +6,12 @@ from rest_framework.permissions import AllowAny
 
 class RegisterUserView(generics.CreateAPIView):
     queryset = User.objects.all()
-    permission_classes = [AllowAny] # Anyone can register!
+    permission_classes = [AllowAny] # Anyone can register
     serializer_class = UserRegistrationSerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    # We will restrict this later, but for now allow anyone to view/edit
     permission_classes = [permissions.AllowAny] 
 
 class InventoryItemViewSet(viewsets.ModelViewSet):
@@ -20,15 +19,17 @@ class InventoryItemViewSet(viewsets.ModelViewSet):
     serializer_class = InventoryItemSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    # This is an enterprise feature: Automatically set the owner to the logged-in user!
+    # Automatically set the owner to the logged-in user
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def perform_destroy(self, instance):
+        # Check if the person deleting is the owner
+        if instance.owner != self.request.user:
+            raise exceptions.PermissionDenied("You do not have permission to delete this item.")
+        instance.delete()
+
 class StockAuditViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    We use ReadOnlyModelViewSet because audit logs should NEVER be manually 
-    created, updated, or deleted via standard API calls. They are an unchangeable history.
-    """
     queryset = StockAudit.objects.all().order_by('-timestamp')
     serializer_class = StockAuditSerializer
     permission_classes = [permissions.IsAuthenticated]
