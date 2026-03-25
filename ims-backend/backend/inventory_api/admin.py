83d22e49e5e6072cfe76c73aaef91e2e1d6dbdd4
JackScoble <jack.scoble@ada.ac.uk>
2026-03-25 15:00:21 +0000

    Register DailyStockSnapshot to the admin site

--- ims-backend/backend/inventory_api/admin.py
+++ ims-backend/backend/inventory_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, InventoryItem, StockAudit
+from .models import Category, InventoryItem, StockAudit, DailyStockSnapshot
 
 admin.site.register(Category)
 admin.site.register(InventoryItem)
 admin.site.register(StockAudit)
+admin.site.register(DailyStockSnapshot)
